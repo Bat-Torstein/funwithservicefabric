6bc6b048ad2b5435ef54434522d7606c7f036c81
Torstein Bjørgan <torsteib@gmail.com>
2017-12-24 15:44:19 +0100

    setup projects

--- WcfService/Program.cs
+++ WcfService/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace WcfService
 {
@@ -6,7 +10,6 @@ namespace WcfService
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
